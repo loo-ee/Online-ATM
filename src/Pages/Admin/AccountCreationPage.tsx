70da80ba204324d1b97ae77cea7281ba3beb09d6
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2022-12-22 20:44:07 +0800

    Move RequestCard back into AccountCreationPage

--- src/Pages/Admin/AccountCreationPage.tsx
+++ src/Pages/Admin/AccountCreationPage.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { AdminContext } from '../../contexts/AdminContext';
-import RequestCard from './RequestCard';
+import { AccountRequest } from '../../util/systemConfig';
 
 interface Prop {}
 
@@ -15,6 +15,35 @@ const AccountCreationPage: React.FC<Prop> = ({}) => {
         {Admin?.requests.map((req) => (
           <RequestCard key={req.username} requestBody={req} />
         ))}
+      </div>
+    </div>
+  );
+};
+
+interface RequestCardProp {
+  requestBody: AccountRequest;
+}
+
+const RequestCard: React.FC<RequestCardProp> = ({ requestBody }) => {
+  const bgColor = {
+    BDO: 'bg-blue-900',
+    BPI: 'bg-red-900',
+  };
+
+  return (
+    <div
+      className={
+        'my-3 p-4 text-white rounded-lg phone:w-[150px] tablet:w-[300px] ' +
+        bgColor[requestBody.bank as keyof typeof bgColor]
+      }
+    >
+      <div>
+        <span className="text-2xl">Account Creation</span>
+      </div>
+
+      <div className="flex flex-col mt-2">
+        <span>Bank: {requestBody.bank}</span>
+        <span>Name: {requestBody.username}</span>
       </div>
     </div>
   );
