f77eb63dd7a3cc3f429c2209e86887392d8fda8c
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2022-12-23 21:28:49 +0800

    Create message functionality in PasswordChange component

--- src/Pages/Bank/PasswordChange.tsx
+++ src/Pages/Bank/PasswordChange.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { createChangePinRequest } from '../../adapter/userAdapter';
+import { Message } from '../../util/Message';
 import NumPad from '../../util/NumPad';
 import { AccountModel, ChangePinRequest } from '../../util/systemConfig';
 import BankPageHeader from './BankgPageHeader';
@@ -9,36 +10,61 @@ interface Prop {
 }
 
 const PasswordChange: React.FC<Prop> = ({ account }) => {
+  const [pin, setPin] = useState(0);
   const [headerText, setHeaderText] = useState('Verify login');
   const [arePasswordsMatched, passWordsAreMatched] = useState(false);
+  const [isReadyForSubmission, setIsReadyForSubmission] = useState(false);
+  const messageField = useRef<HTMLInputElement>(null);
 
   const validatePin = (pin: number) => {
     if (!account) return;
 
     if (account.pin == pin) {
       setHeaderText('Change Pin');
+      setPin(pin);
       passWordsAreMatched(true);
     } else setHeaderText("Pins don't match!");
   };
 
-  const changePin = async (pin: number) => {
-    if (!account) return;
+  const prepareToChangePin = async (pin: number) => {
+    setPin(pin);
+    setIsReadyForSubmission(true);
+  };
+
+  const changePin = async () => {
+    if (!account || !messageField.current) return;
 
     const request: ChangePinRequest = {
       accountNumber: account.accountNumber,
       newPin: pin,
       title: 'Change pin UwU',
-      body: 'Please sir UwUUUUUUUUU',
+      body: messageField.current.value,
     };
 
     await createChangePinRequest(request);
+    setIsReadyForSubmission(false);
     setHeaderText('Pin change request submitted to admin');
   };
 
   return (
     <div className="flex flex-col items-center">
       <BankPageHeader headerText={headerText} />
-      <NumPad mainOperation={!arePasswordsMatched ? validatePin : changePin} />
+
+      {arePasswordsMatched ? (
+        <>
+          {isReadyForSubmission && (
+            <Message
+              account={account}
+              messageField={messageField}
+              mainOperation={changePin}
+              preparatoryOperation={setIsReadyForSubmission}
+            />
+          )}
+          <NumPad mainOperation={prepareToChangePin} />
+        </>
+      ) : (
+        <NumPad mainOperation={validatePin} />
+      )}
     </div>
   );
 };
