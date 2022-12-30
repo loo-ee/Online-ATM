d2d991c38854d06239e9e42d38bfaeeb76e78dde
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2022-12-30 22:36:13 +0800

    Add 'Go Back' and 'Home' buttons in Controls component

--- src/Pages/Home/Controls/Controls.tsx
+++ src/Pages/Home/Controls/Controls.tsx
@@ -10,31 +10,65 @@ const Controls: React.FC<Prop> = ({}) => {
     navigator(baseUrl + 'messages/');
   };
 
-  return (
-    <div className="flex phone:flex-row laptop:flex-col phone:w-64 laptop:w-32 rounded-lg h-80 items-center justify-evenly p-3">
-      <div
-        className="w-24 h-32 border-4 border-black items-center bg-u_gray p-3 rounded-lg"
-        onClick={goToMessages}
-      >
-        <img
-          src={
-            new URL('../../../assets/images/message.png', import.meta.url).href
-          }
-          className=""
-          alt=""
-        />
-        <span>Messages</span>
-      </div>
+  const goToSettings = () => {
+    console.log('setttings');
+  };
 
-      <div className="w-24 h-32 border-4 border-black bg-u_gray items-center p-3 rounded-lg">
-        <img
-          src={
-            new URL('../../../assets/images/settings.png', import.meta.url).href
-          }
-          alt=""
-        />
-        <span>Settings</span>
-      </div>
+  const goBack = () => {
+    navigator(-1);
+  };
+
+  const goToHome = () => {
+    navigator(baseUrl + 'feed/');
+  };
+
+  return (
+    <div className="flex phone:flex-row laptop:flex-col phone:w-96 laptop:w-32 rounded-lg phone:h-[150px] laptop:h-[600px] items-center justify-evenly p-3">
+      <SettingsButton
+        text="Messages"
+        imgSrc="message.png"
+        operation={goToMessages}
+      />
+
+      <SettingsButton
+        text="Settings"
+        imgSrc="settings.png"
+        operation={goToSettings}
+      />
+
+      <SettingsButton
+        text="Go Back"
+        imgSrc="turn-back.png"
+        operation={goBack}
+      />
+
+      <SettingsButton text="Home" imgSrc="home.png" operation={goToHome} />
+    </div>
+  );
+};
+
+interface SettingsButtonProp {
+  text: string;
+  imgSrc: string;
+  operation: () => void;
+}
+
+export const SettingsButton: React.FC<SettingsButtonProp> = ({
+  text,
+  imgSrc,
+  operation,
+}) => {
+  return (
+    <div
+      className="text-center phone:w-20 phone:h-28 laptop:w-24 laptop:h-32 border-4 border-black items-center bg-u_gray p-3 rounded-lg"
+      onClick={operation}
+    >
+      <img
+        src={new URL(`../../../assets/images/${imgSrc}`, import.meta.url).href}
+        className=""
+        alt=""
+      />
+      <span className="phone:text-xs laptop:text-[15px]">{text}</span>
     </div>
   );
 };
