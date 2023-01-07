7f258b35a8676ac16312a4d7a1d1e1e29b28c560
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-07 21:32:13 +0800

    Design AccountEditPage component

--- src/Pages/Admin/AccountEditPage.tsx
+++ src/Pages/Admin/AccountEditPage.tsx
@@ -13,11 +13,11 @@ const AccountEditPage: React.FC<Prop> = ({}) => {
   };
 
   return (
-    <div className="phone:w-[250px] laptop:w-[700px] mt-10 flex flex-col">
+    <div className="phone:w-[250px] laptop:w-[700px] mt-10 flex flex-col items-center">
       <span className="laptop:text-3xl self-center">Account Edit Page</span>
       <div
         className={
-          'p-4 flex flex-row justify-between text-white items-baseline rounded-lg border-black border-4 laptop:mt-4' +
+          'p-4 flex flex-row justify-between text-white items-baseline rounded-lg border-black border-4 laptop:mt-4 laptop:w-[500px]' +
           bgColor[System?.accountSelected?.bank as keyof typeof bgColor]
         }
       >
@@ -29,11 +29,54 @@ const AccountEditPage: React.FC<Prop> = ({}) => {
 
       <div
         className={
-          'p-4 flex flex-col text-white rounded-lg border-black border-4 laptop:mt-4' +
+          'p-4 flex flex-col text-white rounded-lg border-black border-4 laptop:mt-4 laptop:w-[500px] items-center' +
           bgColor[System?.accountSelected.bank as keyof typeof bgColor]
         }
       >
-        <span></span>
+        <div className="flex flex-row items-center phone:my-1 laptop:my-3 justify-between w-[350px]">
+          <label
+            htmlFor="name"
+            className="phone:text-sm tablet:text-md laptop:text-lg"
+          >
+            Name:{' '}
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder={System?.accountSelected.name}
+            className="text-black phone:p-1 laptop:p-3 rounded phone:w-32 phone:text-xs laptop:text-lg laptop:w-auto"
+          />
+        </div>
+
+        <div className="flex flex-row items-center phone:my-1 laptop:my-3 justify-between w-[350px]">
+          <label
+            htmlFor="balance"
+            className="phone:text-sm tablet:text-md laptop:text-lg"
+          >
+            Balance:{' '}
+          </label>
+          <input
+            type="number"
+            id="balance"
+            placeholder={System?.accountSelected.balance.toString()}
+            className="text-black phone:p-1 laptop:p-3 rounded phone:w-32 phone:text-xs laptop:text-lg laptop:w-auto"
+          />
+        </div>
+
+        <div className="flex flex-row items-center phone:my-1 laptop:my-3 justify-between w-[350px]">
+          <label
+            htmlFor="pin"
+            className="phone:text-sm tablet:text-md laptop:text-lg"
+          >
+            Pin:{' '}
+          </label>
+          <input
+            type="number"
+            id="pin"
+            placeholder={System?.accountSelected.pin.toString()}
+            className="text-black phone:p-1 laptop:p-3 rounded phone:w-32 phone:text-xs laptop:text-lg laptop:w-auto"
+          />
+        </div>
       </div>
     </div>
   );
