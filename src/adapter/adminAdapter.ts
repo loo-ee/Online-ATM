a0295e9d5d0946e458d3345ca10ab8aeea66eed4
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-16 19:59:08 +0800

    Test

--- src/adapter/adminAdapter.ts
+++ src/adapter/adminAdapter.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AccountModel, AccountRequest } from '../util/systemConfig';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://43.206.230.229/',
   headers: {
     'Content-type': 'application/json',
   },
