020b2fe5bd8a27fc9cffcd8b29a458266520b18e
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-17 21:18:03 +0800

    Prepare for railway hosting

--- src/adapter/systemAdapter.ts
+++ src/adapter/systemAdapter.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BankModel } from '../util/systemConfig';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://online-atm.up.railway.app/',
   headers: {
     'Content-type': 'application/json',
   },
