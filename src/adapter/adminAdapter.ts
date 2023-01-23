c64295be880ca7e3e2d7770fa63efa4445c3d0fe
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-23 18:35:10 +0800

    Change backend source from localhost to railway, add vercel.json

--- src/adapter/adminAdapter.ts
+++ src/adapter/adminAdapter.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AccountModel, AccountRequest } from '../util/systemConfig';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://online-atm.up.railway.app/',
   headers: {
     'Content-type': 'application/json',
   },
