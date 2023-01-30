338c4542d2a3cb10fa1b463480b261d033266dda
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-30 22:18:31 +0800

    Testing fix from GH

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/Online-ATM/',
-// });
+export default defineConfig({
+  // plugins: [react()],
+  // base: '/Online-ATM/',
+});
